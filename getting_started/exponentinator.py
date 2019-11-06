7fd41931644c42a3061ff4c4259fe00ea75c22fb
RWEngelbrecht <rigardt@brechtburg.co.za>
2019-11-06 17:08:58 +0200

    made exponant function, forgot python can do math

--- getting_started/exponentinator.py
+++ getting_started/exponentinator.py
@@ -1,8 +1,27 @@
 
 import math
 
+
+def calc_exp(base, exp):
+	ans = 1
+	for i in range(exp):
+		ans *= base
+	return ans
+
+
 prob = input("Enter what you want exponated (ie 2^2): ")
 
-if prob.
-base = prob.split("^")
-print(base)
+if prob.find("^"):
+	base_exp = prob.split("^")
+
+base_num = int(base_exp[0])
+exp_num = int(base_exp[1])
+
+print("using created function:	"+str(calc_exp(base_num, exp_num)))
+
+print("""\
+
+OR
+	""")
+
+print("using built-in math:	"+str(base_num**exp_num))
