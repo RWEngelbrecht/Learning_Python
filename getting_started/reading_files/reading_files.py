487ae29640320af565b2ba65ba0f64ec61b2f1ec
rengelbr <rengelbr@student.wethinkcode.co.za>
2019-11-07 10:05:48 +0200

    reading from files

--- getting_started/reading_files/reading_files.py
+++ getting_started/reading_files/reading_files.py
@@ -1,12 +1,22 @@
+## PLAYING WITH READING FROM FILES
+
 #open("test.txt", "r")   #read
 #open("test.txt", "w")   #write
 #open("test.txt", "r+")  #r + w
 #open("test.txt", "a")   #apend
 
-test_file = open("reading_files/test.txt", "r")
+test_file = open("getting_started/reading_files/test.txt", "r")
 
 try:
-    print(test_file.readline())
+    lines = test_file.readlines()     #read() returns all lines as single string. readlines() returns array. I mean list.
+    print(lines)
+    for line in lines:
+        if "too" in line and "boogaloo" in line:
+            print(line+" (the sequel's sequel)")
+        elif "too" in line:
+            print(line+" (the sequel)")
+        else:
+            print(line)
 except:
     print("nope")
 
